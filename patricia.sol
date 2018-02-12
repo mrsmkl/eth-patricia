d06811b2c54550cfc8fc9896824adb74a4e0e567
Sami Mäkelä <mrsmkl@gmail.com>
2018-02-12 11:03:31 -0800

    unmangling hex prefix

--- patricia.sol
+++ patricia.sol
@@ -28,48 +28,101 @@ contract Patricia {
       wantHash = root;
    }
    
+   function bytesToBytes32(bytes rlp) internal pure returns (bytes32) {
+       bytes32 res;
+       assembly {
+           res := mload(add(32,rlp))
+       }
+       return res;
+   }
+   
    function rlpArrayLength(bytes rlp) internal pure returns (int) {
       uint8 elem = uint8(rlp[0]);
       if (elem < 192 || elem >= 247) return -1;
       else return elem - 192;
    }
    
-   function rlpByteLength(bytes rlp, uint idx) internal pure returns (int) {
+   function rlpByteLength(bytes rlp, uint idx) internal pure returns (uint) {
       uint8 elem = uint8(rlp[idx]);
       if (elem < 128) return 1;
-      if (elem >= 183) return -1;
+      if (elem >= 183) {
+          /// umm this can be an array
+          uint n = elem - 192;
+          uint res = 1;
+          for (uint i = 0; i < n; i++) {
+              uint len = rlpByteLength(rlp, idx);
+              idx += len + 1;
+              res += len + 1;
+          }
+          return res;
+      }
       else return elem - 128;
    }
    
-   function sliceBytes(bytes b, uint idx, uint len) internal returns (bytes) {
-       
+   function sliceBytes(bytes b, uint idx, uint len) internal pure returns (bytes) {
+      bytes memory res = new bytes(len);
+      for (uint i = 0; i < len; i++) res[i] = b[idx+i];
+      return res;
    }
    
-   function rlpFindBytes(bytes rlp, int n) internal returns (bytes) {
+   function slice(uint8[] storage b, uint idx, uint len) internal view returns (uint8[]) {
+      uint8[] memory res = new uint8[](len);
+      for (uint i = 0; i < len; i++) res[i] = b[idx+i];
+      return res;
+   }
+   
+   function rlpFindBytes(bytes memory rlp, uint n) internal pure returns (bytes) {
       uint idx = 1;
       for (uint i = 0; i < n-1; i++) {
-         int len = rlpByteLength(rlp, idx);
+         uint len = rlpByteLength(rlp, idx);
          require (len >= 0);
          idx += len + 1;
       }
-      int len = rlpByteLength(rlp, idx);
+      len = rlpByteLength(rlp, idx);
       require (len >= 0);
       return sliceBytes(rlp, idx, len);
    }
    
+   // unmangle HP encoding to boolean value and nibbles
+   function unhp(bytes b) internal pure returns (bool tval, uint8[] res) {
+       uint8 elem = uint8(b[0]);
+       uint8 flag = elem/16;
+       tval = (flag & 0x2 == 1);
+       bool even = (flag & 0x1 == 0);
+       uint len = ((b.length-1) / 2) + (even ? 0 : 1);
+       res = new uint8[](len);
+       uint idx = 0;
+       if (!even) {
+           idx = 1;
+           res[0] = elem&0xf;
+       }
+       for (uint i = 1; i < b.length; i++) {
+          uint8 elem1 = uint8(b[i]);
+          res[idx+i*2] = (elem1 / 16) & 15;
+          res[idx+i*2+1] = elem1 & 15;
+       }
+   }
+   
    function stepProof(bytes p) public {
+      require(state == State.UNFINISHED);
       require(wantHash == keccak256(p));
       // The key cannot be found here
       if (p.length == 0) {
-         wantHash = 0;
-         state = NOTFOUND;
+         state = State.NOTFOUND;
          return;
       }
       // Branch
       // p[0] == 192+17 == 209
       if (rlpArrayLength(p) == 17) {
          if (key.length == 0) {
-           bytes res = rlpFindBytes(16);
+           found = rlpFindBytes(p, 16);
+           state = State.FOUND;
+         }
+         else {
+             bytes memory child = rlpFindBytes(p, uint(uint8(key[0])));
+             key = slice(key, 1, key.length-1);
+             if (child.length == 32) wantHash = bytesToBytes32(child);
+             else wantHash = keccak256(child);
          }
       }
       // Leaf or extension
